e32e109dde1bdfb82ffc49330a2f7225b2356b05
Lun Sovathana <lunsovathana@luns-mac-mini.local>
2015-11-13 13:55:46 +0700

    Initial Commit

---
+++ GCDExercise/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCDExercise
+//
+//  Created by Lun Sovathana on 11/13/15.
+//  Copyright © 2015 Lun Sovathana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
